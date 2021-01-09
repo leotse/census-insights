f547039c2e19d1a4abc9482f3a1e5e856a585be8
leotse <leocltse@gmail.com>
2021-01-09 11:33:42 -0500

    Lat/lng dissemination area query.

--- src/api/main.py
+++ src/api/main.py
@@ -1,10 +1,18 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from services.dissemination_area import query_dissemination_area
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/stats")
@@ -13,5 +21,5 @@ def read_stats(lat: float, lon: float):
 
 
 @app.get("/api/dissemination-area")
-def get_dissemination_area(lat: float, lon: float):
-    return query_dissemination_area(lat, lon)
+def get_dissemination_area(lng: float, lat: float):
+    return query_dissemination_area(lng, lat)
--- src/services/dissemination_area.py
+++ src/services/dissemination_area.py
@@ -1,14 +1,18 @@
 from geoalchemy2.shape import to_shape
 from shapely.geometry import mapping
+from sqlalchemy import func
 
 from models import Session
 from models.dissemination_area import DisseminationArea
 
 
-def query_dissemination_area(lat: float, lon: float):
+def query_dissemination_area(lng: float, lat: float):
     session = Session()
     try:
-        da = session.query(DisseminationArea).first()
+        filter = func.ST_Contains(DisseminationArea.geometry, f"SRID=4326;POINT({lng} {lat})")
+        da = session.query(DisseminationArea).filter(filter).first()
+        if not da:
+            return None
         return {
             "id": da.id,
             "province_name": da.province_name,
