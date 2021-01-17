1daeb4398770684c97320208f1b2223ea7f4a7a5
leotse <leocltse@gmail.com>
2021-01-16 21:06:47 -0500

    Aggregate stats by list of latlngs.

--- src/services/stats.py
+++ src/services/stats.py
@@ -10,18 +10,61 @@ from models.dissemination_area import DisseminationArea
 
 @use_db
 def query_stats_by_lnglats(lnglats: List[LngLat], *, session=None):
-    query = session.query(AgeGroup)
+    # sum age groups
+    query = session.query(
+        func.sum(AgeGroup.total).label("total"),
+        func.sum(AgeGroup.age_0_to_4).label("age_0_to_4"),
+        func.sum(AgeGroup.age_5_to_9).label("age_5_to_9"),
+        func.sum(AgeGroup.age_10_to_14).label("age_10_to_14"),
+        func.sum(AgeGroup.age_15_to_19).label("age_15_to_19"),
+        func.sum(AgeGroup.age_20_to_24).label("age_20_to_24"),
+        func.sum(AgeGroup.age_25_to_29).label("age_25_to_29"),
+        func.sum(AgeGroup.age_30_to_34).label("age_30_to_34"),
+        func.sum(AgeGroup.age_35_to_39).label("age_35_to_39"),
+        func.sum(AgeGroup.age_40_to_44).label("age_40_to_44"),
+        func.sum(AgeGroup.age_45_to_49).label("age_45_to_49"),
+        func.sum(AgeGroup.age_50_to_54).label("age_50_to_54"),
+        func.sum(AgeGroup.age_55_to_59).label("age_55_to_59"),
+        func.sum(AgeGroup.age_60_to_64).label("age_60_to_64"),
+        func.sum(AgeGroup.age_65_to_69).label("age_65_to_69"),
+        func.sum(AgeGroup.age_70_to_74).label("age_70_to_74"),
+        func.sum(AgeGroup.age_75_to_79).label("age_75_to_79"),
+        func.sum(AgeGroup.age_80_to_84).label("age_80_to_84"),
+        func.sum(AgeGroup.age_85_plus).label("age_85_plus"),
+    )
+
+    # join AgeGroup and DisseminationArea tables..
     query = query.filter(DisseminationArea.dissemination_area_id == AgeGroup.geo_code)
 
-    # build location filteres
+    # by input locations
     location_filters = [
         func.ST_Contains(DisseminationArea.geometry, f"SRID=4326;POINT({lnglat.lng} {lnglat.lat})")
         for lnglat in lnglats
     ]
     query = query.filter(or_(*location_filters))
 
-    print(query.all())
-    return {"hello": 123}
+    aggregated = query.first()
+    return {
+        "total": aggregated.total,
+        "age_0_to_4": aggregated.age_0_to_4,
+        "age_5_to_9": aggregated.age_5_to_9,
+        "age_10_to_14": aggregated.age_10_to_14,
+        "age_15_to_19": aggregated.age_15_to_19,
+        "age_20_to_24": aggregated.age_20_to_24,
+        "age_25_to_29": aggregated.age_25_to_29,
+        "age_30_to_34": aggregated.age_30_to_34,
+        "age_35_to_39": aggregated.age_35_to_39,
+        "age_40_to_44": aggregated.age_40_to_44,
+        "age_45_to_49": aggregated.age_45_to_49,
+        "age_50_to_54": aggregated.age_50_to_54,
+        "age_55_to_59": aggregated.age_55_to_59,
+        "age_60_to_64": aggregated.age_60_to_64,
+        "age_65_to_69": aggregated.age_65_to_69,
+        "age_70_to_74": aggregated.age_70_to_74,
+        "age_75_to_79": aggregated.age_75_to_79,
+        "age_80_to_84": aggregated.age_80_to_84,
+        "age_85_plus": aggregated.age_85_plus,
+    }
 
 
 @use_db
